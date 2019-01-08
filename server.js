171c725dce5dc6dac958bebe3c2922078c73bb1c
Witsarut Sarai <raviel_007@hotmail.com>
2019-01-08 14:09:52 +0700

    Fix ploblem of 2 function can run w/o err

--- server.js
+++ server.js
@@ -1,28 +1,45 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var fs = require('fs');
- var app = express();
-app.use(bodyParser.json());
+// var express = require('express');
 
-var server  = app.listen(8080, function() {
-    var port = server.address().port
-    console.log("Sample code for RESTful API run at ", port)
-});
-app.get('/listUser',function(req,res){
-    fs.readFile(__dirname + "/" + "users.json", 'utf8',function(err,data){
-        console.log(data);
-        res.end(data);
-    });
-});
+// var fs = require('fs');
+//  var app = express();
+
+
+// var server  = app.listen(8080, function() {
+//     var port = server.address().port
+//     console.log("Sample code for RESTful API run at ", port)
+// });
+// app.get('/listUser',function(req,res){
+//     fs.readFile(__dirname + "/" + "users.json", 'utf8', function(err,data) {
+//         console.log(data.name);
+//         res.end(data);
+//     });
+// });
+
+var express = require('express');
+var app = express();
+var fs = require("fs");
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.get('/listUsers', function (req, res) {
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+      console.log( data );
+      res.end( data );
+   });
+})
+
+var server = app.listen(8080, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
+
 app.post('/addUser', function(req,res) {
     var json = req.body;
 
     fs.readFile(__dirname + "/" + "users.json",'utf8', function (err,data) {
         data = JSON.parse(data);
-        data["user4"] = req.body;
+        data["4"] = req.body;
         console.log(data);
         res.end(JSON.stringify(data));
     });
 });
-
-
