83d495eeca76cdeada19bf19abe4385fe907f501
Witsarut Sarai <raviel_007@hotmail.com>
2019-01-08 13:57:14 +0700

    add function addUser & listUser

--- server.js
+++ server.js
@@ -8,14 +8,21 @@ var server  = app.listen(8080, function() {
     var port = server.address().port
     console.log("Sample code for RESTful API run at ", port)
 });
-
+app.get('/listUser',function(req,res){
+    fs.readFile(__dirname + "/" + "users.json", 'utf8',function(err,data){
+        console.log(data);
+        res.end(data);
+    });
+});
 app.post('/addUser', function(req,res) {
     var json = req.body;
 
     fs.readFile(__dirname + "/" + "users.json",'utf8', function (err,data) {
         data = JSON.parse(data);
-        data[] = req.body;
+        data["user4"] = req.body;
         console.log(data);
         res.end(JSON.stringify(data));
-    })
+    });
 });
+
+
