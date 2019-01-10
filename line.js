ac6f55966d3130820084aade21d5cfa206f5cd20
Chayanont Jongmekwamsuk <chayanont.jon@student.mahidol.ac.th>
2019-01-11 02:36:20 +0700

    almost done

--- line.js
+++ line.js
@@ -14,46 +14,65 @@ let count = 0
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+// var options = { method: 'GET',
+//   url: 'http://localhost:4000/webhook',
+//   headers: 
+//    { 'Postman-Token': '34285dc8-eddd-4c6b-9f02-4c5524eaa945',
+//      'cache-control': 'no-cache' } };
+
 // Push
 app.get('/webhook', (req, res) => {			
 	// push block
-	let msg = 'Hello'				// ; can or cannnot added in afterward
-	push(msg)
+	//let msg = 'Hello'				// ; can or cannnot added in afterward
+	//push(msg)
 	res.send(msg)						// for showing when perform
 		
 })
 
+
 // Reply
 app.post('/webhook', (req, res) => {		// WEBHOOK send to bot is always post method
+	console.log('webhook passed')
 	// reply block							// from users
+	// request(options, function (error, response, body) {
+  	// if (error) throw new Error(error);
+		
+	//   body = JSON.parse(body)
+
+	//   let temp = body.temperature[length-1]
+	//   let humidity = body.humidity[length-1]
+	//   let time = body.timestamp
+  
+	// });
+	// let reply_token = req.body.events[0].replyToken     // get event 0 (the first array) that we want to get reply_token
+	// let echo = req.body.events[0].message.text
 	if(req.body.events[0].type == 'beacon')
 	{
-		// let mess = 'Welcome'
-		// let reply_token = req.body.events[0].replyToken     // get event 0 (the first array) that we want to get reply_token
-		// let msg = JSON.stringify(req.body)
-		// if(count <0){ count = 0}
-		// if(req.body.events[0].beacon.type == 'enter'){
-		// 	let body = 'enter'
-		// 	//push(mess)
-		// 	sendbeacon('enter', body)
-		// 	//console.log('Pin')
-		// 	count = count + 1
-		// 	//console.log('enter = ' + count)
-		// }
-		// else if(req.body.events[0].beacon.type == 'leave'){
-		// 	let body = 'leave'
-		// 	sendbeacon('leave', body)
-		// 	console.log('Pout')
-		// 	count = count - 1
-		// 	console.log('leave = ' + count)
-		// }
-		// if(count > 2){
-		// 	let noti = 'There are too many people, please wait a moment'
-		// 	push(noti)
-		// 	// if(count < 2 && count > 0){
-		// 	// 	reply(reply_token, )
-		// 	// }
-		// }
+		 let greet = 'Welcome'
+		 let msg = JSON.stringify(req.body)
+		 let reply_token = req.body.events[0].replyToken     // get event 0 (the first array) that we want to get reply_token
+		 //let echo = req.body.events[0].message.text
+		 if(count < 0)  count = 0
+		 if(req.body.events[0].beacon.type == 'enter'){
+		 	let ok = 'enter'
+			greeting(reply_token, greet)
+		 	sendbeacon('enter', ok)
+			console.log('Pin')
+			count = count + 1
+			console.log('enter = ' + count)
+		}
+		else if(req.body.events[0].beacon.type == 'leave'){
+			let ok = 'leave'
+			sendbeacon('leave', ok)
+			console.log('Pout')
+			count = count - 1
+			console.log('leave = ' + count)
+		}
+		if(count > 2){
+			let msg = 'There are too many people, please wait a moment'
+			reply(reply_token, msg)
+			casting(reply_token, msg)
+		}
     }
 	else if(req.body.events[0].message.text == 'Admin_Mon'){
 		let reply_token = req.body.events[0].replyToken
@@ -61,48 +80,43 @@ app.post('/webhook', (req, res) => {		// WEBHOOK send to bot is always post meth
 		let body = JSON.stringify({
 			replyToken: reply_token,
 		})
-		//reply(reply_token, msg)
-		show(reply_token, msg)
-        //requestdata(body)
+		console.log('Wordin')
+		reply(reply_token, msg)
+        sendreply(body)
 	}
 	res.sendStatus(200)
 })
 
-/*
-function requestdata(body){
+
+function sendreply(body){
     request.post({
-        url: 'https://63d407c3.ngrok.io/receiveBeacon/showdb',
+        url: 'https://13e202fb.ngrok.io/receiveBeacon/showdb',
         headers: HEADERS,
         body: body
     }, (err, res, body) => {
 		console.log('req')
 	})
-}*/
+}
 
-function sorry(reply_token, ) {
+
+function casting(reply_token,msg) {
+	console.log('casting')
 	let body = JSON.stringify({
 		// push body
 		replyToken: reply_token,
 		messages: [{
 			type: 'text',
 			text: msg
+		},
+		{
+			type: 'sticker',
+			packageId: '1',
+			stickerId: '106'
 		}]
-	
 	  })
   curl('reply', body)
 }
-/*
-function data(method, ok){
-	request.post({
-		url: 'https://api.line.me/v2/bot/message/' + method,
-		headers: HEADERS,
-		ok: ok								// must tranform to json 
-	}, (err, res, body) => {
-		//console.log(data)
-		console.log('send')
-	})
-}
-*/
+
 /*
 function push(noti) {					// similar to notification (push message to users)
 	let body = JSON.stringify({
@@ -117,130 +131,21 @@ function push(noti) {					// similar to notification (push message to users)
 }
 */
 
-function show(reply_token, msg){
-	console.log('Pass show')
-	let body = JSON.stringify({
-		messages: [{
-            type: 'flex',
-            altText: 'Flex Message',
-            contents: {
-              type: 'bubble',
-              header: {
-                type: 'box',
-                layout: 'vertical',
-                contents: [
-                  {
-                    type: 'text',
-                    text: 'Sanam Chandra Palace ',
-                    size: 'xs',
-                    weight: 'bold',
-                    color: '#AAAAAA'
-                  }
-                ]
-              },
-              hero: {
-                type: 'image',
-                url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Chali_Mongkol_Asana.jpg/1200px-Chali_Mongkol_Asana.jpg',
-                flex: 3,
-                align: 'start',
-                size: 'full',
-                aspectRatio: '20:13',
-                aspectMode: 'cover',
-                action: {
-                  type: 'uri',
-                  label: 'Action',
-                  uri: 'https://linecorp.com/'
-                }
-              },
-              body: {
-                type: 'box',
-                layout: 'horizontal',
-                spacing: 'md',
-                contents: [
-                  {
-                    type: 'box',
-                    layout: 'vertical',
-                    flex: 1,
-                    contents: [
-                      {
-                        type: 'image',
-                        url: 'https://static.bhphotovideo.com/explora/sites/default/files/styles/top_shot/public/Color-Temperature.jpg?itok=yHYqoXAf',
-                        gravity: 'bottom',
-                        size: 'sm',
-                        aspectRatio: '4:3',
-                        aspectMode: 'cover'
-                      },
-                      {
-                        type: 'image',
-                        url: 'https://www.clipartmax.com/png/middle/251-2517394_backpacking-package-tour-tourism-cartoon-tourist-cartoon-png.png',
-                        margin: 'md',
-                        size: 'sm',
-                        aspectRatio: '4:3',
-                        aspectMode: 'cover'
-                      }
-                    ]
-                  },
-                  {
-                    type: 'box',
-                    layout: 'vertical',
-                    flex: 2,
-                    contents: [
-                      {
-                        type: 'text',
-                        text: 'Temperature : ',
-                        flex: 1,
-                        size: 'xs',
-                        gravity: 'top',
-                        weight: 'bold'
-                      },
-                      {
-                        type: 'separator',
-                        color: '#2ABA11'
-                      },
-                      {
-                        type: 'text',
-                        text: 'Humidity     :',
-                        flex: 2,
-                        size: 'xs',
-                        gravity: 'center',
-                        weight: 'bold'
-                      },
-                      {
-                        type: 'separator',
-                        color: '#54BC0A'
-                      },
-                      {
-                        type: 'text',
-                        text: 'P In      :',
-                        flex: 2,
-                        size: 'xs',
-                        gravity: 'center',
-                        weight: 'bold',
-                        color: '#141813'
-                      },
-                      {
-                        type: 'separator',
-                        color: '#06BC0B'
-                      },
-                      {
-                        type: 'text',
-                        text: 'P Out    :',
-                        flex: 1,
-                        size: 'xs',
-                        gravity: 'bottom',
-                        weight: 'bold'
-                      }
-                    ]
-                  }
-                ]
-              }
-            }
-          }
-        ]
-  })
-  curl('reply', body)
-}
-
+// function show(reply_token, msg){
+// 	console.log('Pass show')
+// 	let body = JSON.stringify({
+// 		messages: [{
+// 			type: 'text',
+// 			text: 'Temperature = ' + temp
+// 		},
+// 		{
+// 			type: 'text',
+// 			text: 'Humidity = ' + humid
+// 		}
+//         ]
+//   })
+//   curl('reply', body)
+// }
 
 function reply(reply_token, msg) {
 	let body = JSON.stringify({
@@ -250,22 +155,35 @@ function reply(reply_token, msg) {
 			type: 'text',
 			text: msg
 		}]
-	
+
+	  })
+  curl('reply', body)
+  console.log('reply')
+}
+
+
+function greeting(reply_token, greet) {
+	let body = JSON.stringify({
+		// push body
+		replyToken: reply_token,
+		messages: [{
+			type: 'text',
+			text: greet
+		}]
+
 	  })
   curl('reply', body)
 }
 
 function sendbeacon(method, ok){
 	request.post({
-		url: 'http://e4d62859.ngrok.io/receiveData/beacon/' + method,
+		url: 'http://13e202fb.ngrok.io/receiveData/beacon/' + method,
 		headers: HEADERS,
 		ok: ok								// must tranform to json 
 	}, (err, res, body) => {
-		//console.log(data)
 		console.log('send')
 	})
 }
-
 
 function curl(method, body) {						// curl is fn post request value to api depend on pushing or replying by define parameter for fixing
 	request.post({
@@ -276,7 +194,6 @@ function curl(method, body) {						// curl is fn post request value to api depen
 		console.log('status = ' + res.statusCode)
 	})
 }
-
 
 app.listen(port, hostname, () => {
 	console.log(`Server running at http://${hostname}:${port}/`)		// returning hostname
